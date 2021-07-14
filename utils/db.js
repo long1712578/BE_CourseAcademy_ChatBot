7f2acca4f14dcb3b9c6cb0fe4816b0a578e443ad
TranHuynh1999 <57291457+TranHuynh1999@users.noreply.github.com>
2021-07-14 08:51:59 +0700

    update db

--- utils/db.js
+++ utils/db.js
@@ -2,10 +2,21 @@ const util = require('util');
 const mysql = require('mysql');
 const knex = require('knex')({
   client: 'mysql2',
+  // connection: {
+  //   host: 'localhost',
+  //   user: 'root',
+  //   password: 'mysql',
+  //   database: 'course_academy',
+  //   port: 3306
+  // },
   connection: {
-    host: 'localhost',
+    host: 'remotemysql.com',
+    user: 'vO0WORyO3w',
+    password: 'ZQDtk88dCQ',
+    database: 'vO0WORyO3w',
+    host: '127.0.0.1',
     user: 'root',
-    password: 'mysql',
+    password: 'huynh012',
     database: 'course_academy',
     port: 3306
   },
