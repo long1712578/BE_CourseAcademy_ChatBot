1b628e763f0f80d0e18ea752aaaa29974cd0670a
long1712578 <pdlong578@gmail.com>
2021-07-16 07:26:40 +0700

    fix login

--- utils/db.js
+++ utils/db.js
@@ -10,13 +10,13 @@ const knex = require('knex')({
   //   port: 3306
   // },
   connection: {
-    host: 'remotemysql.com',
-    user: 'vO0WORyO3w',
-    password: 'ZQDtk88dCQ',
-    database: 'vO0WORyO3w',
+    // host: 'remotemysql.com',
+    // user: 'vO0WORyO3w',
+    // password: 'ZQDtk88dCQ',
+    // database: 'vO0WORyO3w',
     host: '127.0.0.1',
     user: 'root',
-    password: 'mysql',
+    password: '',
     database: 'course_academy',
     port: 3306
   },
